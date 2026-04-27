fc7ee26837e97c7417b1d3976450d7a1241460da
Tal092 <talhadd1234@gmail.com>
2026-04-27 10:21:39 +0300

    connected to mongodb

---
+++ src/lib/mongodb.js
@@ -0,0 +1,33 @@
+// lib/mongodb.js
+import mongoose from "mongoose";
+
+const MONGODB_URI = process.env.MONGODBURI;
+
+if (!MONGODB_URI) {
+  throw new Error(
+    "Please define the MONGODBURI environment variable inside .env.local",
+  );
+}
+
+let cached = global.mongoose;
+
+if (!cached) {
+  cached = global.mongoose = { conn: null, promise: null };
+}
+
+async function dbConnect() {
+  if (cached.conn) return cached.conn;
+
+  if (!cached.promise) {
+    const opts = { bufferCommands: false };
+    cached.promise = mongoose.connect(MONGODB_URI, opts).then((mongoose) => {
+      console.log(" MongoDB Connected successfully");
+      return mongoose;
+    });
+  }
+
+  cached.conn = await cached.promise;
+  return cached.conn;
+}
+
+export default dbConnect;
