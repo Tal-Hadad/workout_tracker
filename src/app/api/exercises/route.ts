5bd5011c3694a09f5fae77211e6e048771369f0a
Tal092 <talhadd1234@gmail.com>
2026-04-28 14:26:59 +0300

    deleted exercise api content

--- src/app/api/exercises/route.ts
+++ src/app/api/exercises/route.ts
@@ -1,8 +1,3 @@
 import mongoose from "mongoose";
 import dbConnect from "@/lib/mongodb";
 import Exercise from "@/models/Exercise";
-
-export async function GET() {
-  await dbConnect();
-
-  try {}
